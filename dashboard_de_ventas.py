e2408f282dd2ea3f66ceaef503033b3121cc6b04
kilojumper-cell <kilo.jumper@gmail.com>
2026-02-28 12:50:08 -0600

    Update dashboard_de_ventas.py

--- dashboard_de_ventas.py
+++ dashboard_de_ventas.py
@@ -18,7 +18,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 import seaborn as sns
-!pip install streamlit
+#!pip install streamlit
 import streamlit as st
 
 #creamos datos sintéticos realistas
@@ -149,7 +149,7 @@ with col2:
 
 #Gráfico completo en la parte superior
 
-st.plotly_chart(fig_dist,use_container_width=True)
+#st.plotly_chart(fig_dist,use_container_width=True)
 
 # Commented out IPython magic to ensure Python compatibility.
 # %%writefile dashboard_ventas.py
